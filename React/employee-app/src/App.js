e2db087ede4a75cde0556e5c47ee213e92ebf71c
GoranACM <goranacm@gmail.com>
2020-11-24 16:00:47 +1100

    Navigation

--- React/employee-app/src/App.js
+++ React/employee-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Home } from './components/Home';
 import { Department } from './components/Department';
 import { Employee } from './components/Employee';
+import { Navigation } from './components/Navigation';
 
 import Button from 'react-bootstrap/Button';
 
@@ -12,6 +13,15 @@ function App() {
   return (
     <BrowserRouter>
       <div className='container'>
+        <h3 className='m-3 d-flex justify-content-center'>
+          React JS with Web API Demo
+        </h3>
+        <h5 className='m-3 d-flex justify-content-center'>
+          Employee management portal
+        </h5>
+
+        <Navigation />
+
         <Switch>
           <Route path='/' component={Home} exact />
           <Route path='/department' component={Department} />
