b1de5080af44f0288eb4261d3d16ecc0ba65a3fd
GoranACM <goranacm@gmail.com>
2020-11-24 19:24:21 +1100

    Implement POST in React to add in department in DB

--- React/employee-app/src/components/AddDepartmentModal.js
+++ React/employee-app/src/components/AddDepartmentModal.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
-import { Modal, Button, Row, Column, Form } from 'react-bootstrap';
+import { Modal, Button, Row, Col, Form, FormGroup } from 'react-bootstrap';
 
 export class AddDepartmentModal extends Component {
   constructor(props) {
     super(props);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    fetch('https://localhost:44351/api/Department', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        DepartmentID: null,
+        DepartmentName: e.target.DepartmentName.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          alert(result);
+        },
+        (error) => {
+          alert('Failed to add');
+        }
+      );
   }
 
   render() {
@@ -20,7 +44,28 @@ export class AddDepartmentModal extends Component {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div className='container'>{/* Add form fields */}</div>
+          <div className='container'>
+            <Row>
+              <Col sm={6}>
+                <Form onSubmit={this.handleSubmit}>
+                  <Form.Group controlId='DepartmentName'>
+                    <Form.Label>Department Name: </Form.Label>
+                    <Form.Control
+                      type='text'
+                      name='DepartmentName'
+                      required
+                      placeholder='Insert department name...'
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Button variant='primary' type='submit'>
+                      Add
+                    </Button>
+                  </Form.Group>
+                </Form>
+              </Col>
+            </Row>
+          </div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant='danger' onClick={this.props.onHide}>
