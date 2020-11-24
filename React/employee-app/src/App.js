e7e86d8e9e22d1e07e8e88501c59516a499eda13
GoranACM <goranacm@gmail.com>
2020-11-24 15:10:25 +1100

    React setup

---
+++ React/employee-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className='App'>
+      <h2>Hello World!</h2>
+      <Button variant='primary'>Button</Button>
+    </div>
+  );
+}
+
+export default App;
