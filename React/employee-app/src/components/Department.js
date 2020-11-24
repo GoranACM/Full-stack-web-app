1ebec3c9ba69cd306293f5ab7b665576393726f5
GoranACM <goranacm@gmail.com>
2020-11-24 16:27:04 +1100

    Department table with dummy data

--- React/employee-app/src/components/Department.js
+++ React/employee-app/src/components/Department.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import { Table } from 'react-bootstrap';
 
 export class Department extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { deps: [] };
+  }
+
+  componentDidMount() {
+    this.refreshList();
+  }
+
+  refreshList() {
+    this.setState({
+      deps: [
+        { DepartmentID: 1, DepartmentName: 'IT' },
+        { DepartmentID: 2, DepartmentName: 'Support' },
+      ],
+    });
+  }
+
   render() {
+    const { deps } = this.state;
     return (
-      <div className='mt-5 d-flex justify-content-left'>
-        <h3>This is the Department page</h3>
-      </div>
+      <Table className='mt-4' striped bordered hover size='sm'>
+        <thead>
+          <tr>
+            <th>Department ID</th>
+            <th>Department Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {deps.map((dep) => (
+            <tr key={dep.DepartmentID}>
+              <td>{dep.DepartmentID}</td>
+              <td>{dep.DepartmentName}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     );
   }
 }
